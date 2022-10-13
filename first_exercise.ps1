a8609277304ed37c707b661a3a4068307749e61f
Sublimez72 <90970690+Sublimez72@users.noreply.github.com>
2022-10-13 14:43:01 +0200

    Added Comments

--- first_exercise.ps1
+++ first_exercise.ps1
@@ -1,12 +1,15 @@
-
+# Assinging both numbers to variables through user input
 [int]$first_num = Read-Host "First number"
 [int]$second_num = Read-Host "Second number"
 
+
+# The logic that handles all the mathematical operations
 $addition = $first_num+$second_num
 $subtraction = $first_num-$second_num 
 $multiplication = $first_num*$second_num
 $division = $first_num/$second_num
 
+# Outputting to the users screen
 Write-Host "$addition"
 Write-Host "$subtraction"
 Write-Host "$multiplication"
