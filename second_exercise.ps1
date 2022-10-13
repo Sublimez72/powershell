04a046e581ece2d8e57ea2902728170656ab0faf
Sublimez72 <90970690+Sublimez72@users.noreply.github.com>
2022-10-13 14:41:42 +0200

    Added Comments

--- second_exercise.ps1
+++ second_exercise.ps1
@@ -1,7 +1,10 @@
-
+# Assigning paths to variables
 $pre_bak = Read-Host "What folder do you want to backup?"
 $post_bak = Read-Host "Where do you want to place the backup?"
 
+
+# Compression of folder
 Compress-Archive $pre_bak $post_bak
 
+# Output to screen
 Write-Host "Created backup of $pre_bak and placed it in $post_bak"
